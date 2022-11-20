c1c7e856d00f37a8a4b6eaa801bcaf57fc04047d
Ege <cirpanliege@gmail.com>
2022-11-20 12:01:13 +0300

    tenth

--- routes/text.js
+++ routes/text.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getLatestText, recordText } = require('../controllers/text')
+const { getLatestText, recordText } = require('../controllers/Text')
 const router = express.Router();
 
 router.get('/gettext', getLatestText)
